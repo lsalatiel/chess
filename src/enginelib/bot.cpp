efb223a0d3415b720ada9d5f9ca810eb498d12bb
lsalatiel <lucassalatiel1004@gmail.com>
2025-02-25 18:07:58 -0300

    fix no possible move bug

--- src/enginelib/bot.cpp
+++ src/enginelib/bot.cpp
@@ -1,5 +1,6 @@
 #include "include/bot.h"
-#include "../chesslib/include/move.h"
+#include "../chesslib/include/piece.h"
+#include "../chesslib/include/board.h"
 #include <ctime>
 #include <cstdlib>
 
@@ -16,6 +17,9 @@ void Bot::make_move(Board &board, std::unordered_map<int, std::list<int>> possib
     int fromSquare = std::get<0>(move);
     int toSquare = std::get<1>(move);
 
+    if (fromSquare == -1 || toSquare == -1)
+        return;
+
     board.make_move(fromSquare, toSquare, board.squares[fromSquare], possibleMoves[fromSquare], false);
 }
 
@@ -26,6 +30,10 @@ std::tuple<int, int> Bot::pick_random_move(Board &board, std::unordered_map<int,
         if (possibleMoves[i].size() > 0) {
             possibleFromSquares.push_back(i);
         }
+    }
+
+    if (possibleFromSquares.size() == 0) {
+        return std::tuple<int, int>({-1, -1});
     }
 
     int fromSquareIndex = rand() % possibleFromSquares.size();
